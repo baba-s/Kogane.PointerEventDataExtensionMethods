e37e8f9ba07f5d390e02b8b6a9e799808da56330
baba-s <shotababa1206@gmail.com>
2022-11-08 19:31:23 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.PointerEventDataExtensionMethods
+# Kogane Pointer Event Data Extension Methods
+
+PointerEventData 型の拡張メソッド
---
+++ Runtime/PointerEventDataExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.EventSystems;
+
+namespace Kogane
+{
+    public static class PointerEventDataExtensionMethods
+    {
+        public static bool IsMultiTouch( this PointerEventData self )
+        {
+            // https://docs.unity3d.com/ja/2018.4/ScriptReference/EventSystems.PointerEventData-pointerId.html
+            // -1: マウスの左クリック
+            // -2: マウスの右クリック
+            // -3: マウスの中央ボタンクリック
+            //  0: シングルタップ
+            //  1: ダブルタップ
+            //  2: トリプルタップ
+            return 0 < self.pointerId;
+        }
+    }
+}
